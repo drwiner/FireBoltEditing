09596924323cbbb7286b2a81c99678134b43dcd8
brthorne <brthorne@ncsu.edu>
2015-04-01 10:37:45 -0400

    adding firebolt action

---
+++ CinematicModel/CinematicModel/ModelStructure/FireBoltAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CinematicModel
+{
+    
+    public class FireBoltAction  
+    {
+        [XmlAttribute(AttributeName = "startTickParamId")]
+        public int StartTickParamId { get; set; }
+
+
+    }
+}
