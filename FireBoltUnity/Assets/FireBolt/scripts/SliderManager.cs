099e703e6dc6b0653489d3ad3ad61786c86a6f12
Justus Robertson <justusrobertson@gmail.com>
2015-09-21 16:07:17 -0400

    Single keyframe now moving with slider.

--- FireBoltUnity/Assets/FireBolt/scripts/SliderManager.cs
+++ FireBoltUnity/Assets/FireBolt/scripts/SliderManager.cs
@@ -5,9 +5,11 @@ using System.IO;
 public class SliderManager : MonoBehaviour 
 {
     private UnityEngine.UI.Slider slider;
+    private UnityEngine.UI.RawImage thumb;
     private RectTransform sliderRect;
     private bool thumbnailEnabled;
     private Texture2D image;
+    private ElPresidente elP;
 
 	// Use this for initialization
 	void Start () 
@@ -16,27 +18,38 @@ public class SliderManager : MonoBehaviour
         sliderRect = gameObject.GetComponent<RectTransform>();
         thumbnailEnabled = false;
         image = LoadPNG(@"Assets/screens/0.png");
+
+        GameObject elPGO = GameObject.Find("FireBolt");
+        elP = elPGO.GetComponent<ElPresidente>();
+
+        GameObject thumbGO = GameObject.Find("Thumbnail");
+        thumb = thumbGO.GetComponent<UnityEngine.UI.RawImage>();
 	}
 	
-	void OnGUI ()
+	void Update ()
     {
         if (thumbnailEnabled) drawThumbnail();
-        else Debug.Log("GOODBYE WORLD");
     }
 
     public void ThumbnailOn ()
     {
         thumbnailEnabled = true;
+        thumb.color = new Color(255, 255, 255, 255);
     }
 
     public void ThumbnailOff ()
     {
         thumbnailEnabled = false;
+        thumb.color = new Color(255, 255, 255, 0);
     }
 
     private void drawThumbnail ()
     {
-        GUI.DrawTexture(new Rect(10, 10, 60, 60), image);
+        float width = sliderRect.rect.width;
+        float deltaX = width * slider.value;
+        float x = deltaX + sliderRect.position.x - (width / 2);
+        thumb.texture = image;
+        thumb.rectTransform.position = new Vector3(x, sliderRect.position.y + (thumb.rectTransform.rect.height / 1.5f), thumb.rectTransform.position.z);
     }
 
     public static Texture2D LoadPNG(string filePath)
@@ -48,7 +61,7 @@ public class SliderManager : MonoBehaviour
         {
             fileData = File.ReadAllBytes(filePath);
             tex = new Texture2D(2, 2);
-            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            tex.LoadImage(fileData);
         }
         return tex;
     }
